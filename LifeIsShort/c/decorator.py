4dfd970d1eb6a3caea892bf00938a0b00a0c52a4
loopyme <peter@mail.loopy.tech>
2020-03-25 13:45:44 +0800

    fix a typo (I am actually loopy now)

--- LifeIsShort/c/decorator.py
+++ LifeIsShort/c/decorator.py
@@ -2,7 +2,9 @@ words = 'Life is short'
 
 
 def print_before(func):
-    # it is thickly to print here, words will be printed while decorating instead of calling the function
+    # it is almost a trap to print here, words will be printed while decorating instead of calling foo
+    # Be aware that @ is just syntactic sugar, decorating is equivalent to `print_before(foo)`
+
     print(words)
 
     def wrapper(*args, **kw):
